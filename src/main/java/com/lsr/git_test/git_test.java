fa34f1217f71b489ef4ed22d22dedc00d5dc8e4d
Lsr <lsr1450940423@qq.com>
2022-10-08 19:41:12 +0800

    second commit

--- src/main/java/com/lsr/git_test/git_test.java
+++ src/main/java/com/lsr/git_test/git_test.java
@@ -3,5 +3,6 @@ package com.lsr.git_test;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("git test...");
+        System.out.println("git test2");
     }
 }
