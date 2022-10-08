7907179b67c59c8e1f4568bcf27babb2172acb1c
Lsr <lsr1450940423@qq.com>
2022-10-08 19:43:28 +0800

    third commit

--- src/main/java/com/lsr/git_test/git_test.java
+++ src/main/java/com/lsr/git_test/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("git test...");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
